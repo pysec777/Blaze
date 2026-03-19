d6ca4e169fab6b15bb4e2b6def5b4fb397fe70bb
ZombieSlave <miguel.dias@estudantes.ifc.edu.br>
2026-03-19 02:52:38 +0000

    Botao reativo/Blaze2.0 completa

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('Apostar').addEventListener('click', function() {
+    document.getElementById('Foto1').src = 'https://picsum.photos/200?' + Math.random();
+    document.getElementById('Foto2').src = 'https://picsum.photos/200?' + Math.random();
+});
